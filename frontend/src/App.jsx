e8cfb381d8b40d346b4fcd9838571d43ca0155f9
hirodinn <hirebikila1@gmail.com>
2025-12-26 00:33:10 +0300

    add Feed component and integrate it into routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Welcome } from "./components/Welcome";
 import Login from "./components/Login.jsx";
 import Register from "./components/Register.jsx";
 import { Dashboard } from "./components/Dashboard";
+import { Feed } from "./components/Feed.jsx";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/feed" element={<Feed />} />
     </Routes>
   );
 }
