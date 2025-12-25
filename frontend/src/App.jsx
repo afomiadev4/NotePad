ae7e8162bbf4fe546d856dd585a8e99327d410dc
hirodinn <hirebikila1@gmail.com>
2025-12-26 01:03:08 +0300

    add AccountPage component and integrate it into routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Login from "./components/Login.jsx";
 import Register from "./components/Register.jsx";
 import { Folders } from "./components/Folders.jsx";
 import { Feed } from "./components/Feed.jsx";
+import { AccountPage } from "./components/AccountPage.jsx";
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
       <Route path="/register" element={<Register />} />
       <Route path="/folders" element={<Folders />} />
       <Route path="/feed" element={<Feed />} />
+      <Route path="/account" element={<AccountPage />} />
     </Routes>
   );
 }
