65c7f34575a2612bf72ec4b72801c94611115b14
hirodinn <hirebikila1@gmail.com>
2025-12-25 21:03:18 +0300

    add registration functionality and update login and welcome components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import { Welcome } from "./components/Welcome";
 import Login from "./components/Login.jsx";
+import Register from "./components/Register.jsx";
 
 export default function App() {
   return (
     <Routes>
       <Route index element={<Welcome />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   );
 }
