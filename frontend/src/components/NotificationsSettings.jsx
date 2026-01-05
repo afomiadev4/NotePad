0e0937f2628b9399003ce8bb40b71cc6a4a01642
Ruth Kifle <pruthget77@gmail.com>
2026-01-06 02:26:39 +0300

    account page edits

---
+++ frontend/src/components/NotificationsSettings.jsx
@@ -0,0 +1,44 @@
+import { Navigation } from "./Navigation";
+import { useNavigate } from "react-router-dom";
+
+export function NotificationsSettings() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="min-h-screen bg-(--bg-primary) text-(--text-primary) flex">
+      <Navigation />
+
+      <main className="flex-1 lg:ml-64 p-6">
+        {/* Header */}
+        <div className="mb-6">
+          <button
+            onClick={() => navigate(-1)}
+            className="text-(--text-secondary) mn-3"
+            aria-label="Black"
+          >
+            ← Back
+          </button>
+          <h1 className="text-xl font-bold">Notifications</h1>
+        </div>
+
+        {/* Card */}
+        <div className="rounded-xl bg-(--bg-secondary) border border-white/10 p-5 space-y-4">
+          <label className="flex items-center justify-between">
+            <span>New Likes</span>
+            <input type="checkbox" className="accent-(--btn-primary)" />
+          </label>
+
+          <label className="flex items-center justify-between">
+            <span>Shared Posts</span>
+            <input type="checkbox" className="accent-(--btn-primary)" />
+          </label>
+
+          <label className="flex items-center justify-between">
+            <span>New comments</span>
+            <input type="checkbox" className="accent-(--btn-primary)" />
+          </label>
+        </div>
+      </main>
+    </div>
+  );
+}
